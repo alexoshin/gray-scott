9ac71eed2ced2db8a6829a54b5c8161451d18151
Alex Oshin <alexoshin@gatech.edu>
2021-04-09 20:06:14 -0400

    Implement diffusion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var rd;
 function onload() {
     document.body.style.opacity = 1;
-    rd = setup_reaction_diffusion(document.getElementById('canvas'), 100, 100);
+    rd = setup_reaction_diffusion(document.getElementById('canvas'), 200, 200);
     rd.start();
     var instructions = document.getElementById('instructions');
     instructions.innerText = '----- Key commands -----\n';
@@ -27,12 +27,21 @@ function setup_reaction_diffusion(canvas, gridWidth, gridHeight) {
     var interval = 0;
     var show_u = true;
 
+    const dt = 2.5;
+    const h = 1.0;
+
+    const r_u = 0.082;
+    const r_v = 0.041;
+
+    const alpha_u = r_u * dt / (h * h);
+    const alpha_v = r_v * dt / (h * h);
+
     // Initialize the concentration grids with random concentrations
     vars.initialize_grids = function() {
-        for (var y = 0; y < gridHeight + 2; y++) {
+        for (var y = 1; y < gridHeight + 1; y++) {
             u[y] = [];
             v[y] = [];
-            for (var x = 0; x < gridWidth + 2; x++) {
+            for (var x = 1; x < gridWidth + 1; x++) {
                 u[y][x] = Math.random();
                 v[y][x] = Math.random();
             }
@@ -43,6 +52,11 @@ function setup_reaction_diffusion(canvas, gridWidth, gridHeight) {
     // Fill 1-cell boundary around the grid with adjacent values for Neumann
     // method, i.e. the derivative at the boundary is held constant
     function fill_boundary() {
+        u[0] = [];
+        v[0] = [];
+        u[gridHeight + 1] = [];
+        v[gridHeight + 1] = [];
+
         for (var x = 1; x < gridWidth + 1; x++) {
             u[0][x] = u[1][x];
             v[0][x] = v[1][x];
@@ -79,6 +93,34 @@ function setup_reaction_diffusion(canvas, gridWidth, gridHeight) {
 
     // Advance the reaction-diffusion sim one step
     function advance_sim() {
+        diffusion();
+        reaction();
+        fill_boundary();
+    }
+
+    // Diffusion of concentration use simple forward Euler integration
+    function diffusion() {
+        var u_new = [];
+        var v_new = [];
+
+        for (var y = 1; y < gridHeight + 1; y++) {
+            u_new[y] = [];
+            v_new[y] = [];
+
+            for (var x = 1; x < gridWidth + 1; x++) {
+                const L_u = u[y - 1][x] + u[y + 1][x] + u[y][x - 1] + u[y][x + 1] - 4 * u[y][x];
+                const L_v = v[y - 1][x] + v[y + 1][x] + v[y][x - 1] + v[y][x + 1] - 4 * v[y][x];
+
+                u_new[y][x] = u[y][x] + alpha_u * L_u;
+                v_new[y][x] = v[y][x] + alpha_v * L_v;
+            }
+        }
+
+        u = u_new;
+        v = v_new;
+    }
+
+    function reaction() {
 
     }
 
